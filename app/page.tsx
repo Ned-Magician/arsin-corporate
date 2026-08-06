0f7325c2f3e929a79d37bf9f257df3c901c42837
Saeid Nejat <nedjat2021@gmail.com>
2026-08-06 10:53:21 +0300

    refactor: add main landmark to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Services } from "@/components/sections/services/Services";
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Hero />
       <Services />
-    </>
+    </main>
   );
 }
